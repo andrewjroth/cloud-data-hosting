6aef5b7f1f98c1529cc7ec582c19bbe933a0dee2
Andrew J Roth <andrew@andrewjroth.com>
2019-07-12 10:08:16 +0000

    minimum functionality

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # cloud-data-hosting
+
+=== App Config Options ===
+
+Settings can be configured in a file indicated by FLASK_SETTINGS.
+
+* S3_REDIRECT - If True, will redirect object downloads to S3 instead of streaming the content back directly.
+* ROOT_LIST - Dictionary (dict) of display names translated to bucket names for the homepage (root index).
--- main.py
+++ main.py
@@ -1,28 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, Response
 import boto3
 
 
 app = Flask(__name__)
+#app.config.from_object('yourapplication.default_settings')
+app.config.from_envvar('FLASK_SETTINGS')
 
 
 @app.route('/')
 def index():
-    s3 = boto3.resource('s3')
-    buckets = s3.buckets.all()
-    return render_template("page.html", items=buckets)
+    return render_template("index.html", path="/", items=app.config['ROOT_LIST'].keys())
 
 
 @app.route('/<path:path>')
 def get(path):
-    split_path = path.split('/')
-    bucket = split_path[0]
+    client = boto3.client('s3')
+    split_path = path.split('/', 1)
+    bucket = app.config['ROOT_LIST'][split_path[0]]
     prefix = ""
     if len(split_path) > 1:
-        prefix = "/".join(split_path[1:])
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(bucket)
-    objects = bucket.objects.filter(Prefix=prefix)
-    return render_template("page.html", items=objects)
+        prefix = split_path[1]
+        if not prefix.endswith('/'): # return object
+            if app.config['S3_REDIRECT']:
+                response = client.generate_presigned_url("get_object", Params=dict(
+                    Bucket=bucket, Key=prefix))
+                return redirect(response)
+            response = client.get_object(
+                Bucket=bucket,
+                Key=prefix
+            )
+            return Response(response['Body'], mimetype=response['ContentType'])
+    folders = list()
+    files = list()
+    list_objects_args=dict(
+        Bucket=bucket,
+        Delimiter='/',
+        Prefix=prefix,
+    )
+    while True:
+        response = client.list_objects_v2(**list_objects_args)
+        if 'CommonPrefixes' in response:
+            folders.extend(map(lambda x: x['Prefix'][len(prefix):], response['CommonPrefixes']))
+        if 'Contents' in response:
+            files.extend(map(
+                lambda x: dict(name=x['Key'][len(prefix):], last_modified=x['LastModified'], size=x['Size']), 
+                response['Contents']
+            ))
+        if response['IsTruncated']:
+            list_objects_args['ContinuationToken'] = response['NextContinuationToken']
+        else:
+            break
+    return render_template("page.html", path='/'+path, folders=folders, files=files)
 
 
 if __name__ == "__main__":
